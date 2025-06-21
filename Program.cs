5bc229faa264946646274af630c7e4c1dc939a5a
Esra Güngör <esragungor1333@gmail.com>
2025-06-21 17:49:14 +0300

    Fix: Add REnder listen port for deployment

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:10000");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
